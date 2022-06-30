dc6c412bb579c14ce9df18425ec026a8ffaf414d
memoriz2 <ahndjds@gmail.com>
2022-06-30 16:37:18 +0900

    네이버 로그인

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.demo.config.auth.LoginUser;
 import com.example.demo.config.auth.dto.SessionUser;
 import com.example.demo.service.BoardService;
 import lombok.RequiredArgsConstructor;
@@ -7,19 +8,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.servlet.http.HttpSession;
-
 @RequiredArgsConstructor
 @Controller
 public class HomeController {
 
     private final BoardService boardService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("list", boardService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user != null){
             model.addAttribute("userName", user.getName());
